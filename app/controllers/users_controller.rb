2a1a4ff93948b1e929b6366ef9b546d53769436e
vmancode <146589771+vmancode@users.noreply.github.com>
2023-12-08 23:32:19 +0000

    commit2

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,56 @@
+class UsersController < ApplicationController
+  def index
+    matching_users = User.all
+
+    @list_of_users = matching_users.order({ :created_at => :desc })
+
+    render({ :template => "users/index" })
+  end
+
+  def show
+    the_id = params.fetch("path_id")
+
+    matching_users = User.where({ :id => the_id })
+
+    @the_user = matching_users.at(0)
+
+    render({ :template => "users/show" })
+  end
+
+  def create
+    the_user = User.new
+    the_user.fan_id = params.fetch("query_fan_id")
+    the_user.photo_id = params.fetch("query_photo_id")
+
+    if the_user.valid?
+      the_user.save
+      redirect_to("/users", { :notice => "user created successfully." })
+    else
+      redirect_to("/users", { :alert => the_user.errors.full_messages.to_sentence })
+    end
+  end
+
+  def update
+    the_id = params.fetch("path_id")
+    the_user = User.where({ :id => the_id }).at(0)
+
+    the_user.fan_id = params.fetch("query_fan_id")
+    the_user.photo_id = params.fetch("query_photo_id")
+
+    if the_user.valid?
+      the_user.save
+      redirect_to("/users/#{the_user.id}", { :notice => "user updated successfully."} )
+    else
+      redirect_to("/users/#{the_user.id}", { :alert => the_user.errors.full_messages.to_sentence })
+    end
+  end
+
+  def destroy
+    the_id = params.fetch("path_id")
+    the_user = User.where({ :id => the_id }).at(0)
+
+    the_user.destroy
+
+    redirect_to("/users", { :notice => "user deleted successfully."} )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,82 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "users#index"
+
+  # Routes for the Like resource:
+
+  # CREATE
+  post("/insert_like", { :controller => "likes", :action => "create" })
+          
+  # READ
+  get("/likes", { :controller => "likes", :action => "index" })
+  
+  get("/likes/:path_id", { :controller => "likes", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_like/:path_id", { :controller => "likes", :action => "update" })
+  
+  # DELETE
+  get("/delete_like/:path_id", { :controller => "likes", :action => "destroy" })
+
+  #------------------------------
+
+  # Routes for the Follow request resource:
+
+  # CREATE
+  post("/insert_follow_request", { :controller => "follow_requests", :action => "create" })
+          
+  # READ
+  get("/follow_requests", { :controller => "follow_requests", :action => "index" })
+  
+  get("/follow_requests/:path_id", { :controller => "follow_requests", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_follow_request/:path_id", { :controller => "follow_requests", :action => "update" })
+  
+  # DELETE
+  get("/delete_follow_request/:path_id", { :controller => "follow_requests", :action => "destroy" })
+
+  #------------------------------
+
+  # Routes for the Comment resource:
+
+  # CREATE
+  post("/insert_comment", { :controller => "comments", :action => "create" })
+          
+  # READ
+  get("/comments", { :controller => "comments", :action => "index" })
+  
+  get("/comments/:path_id", { :controller => "comments", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_comment/:path_id", { :controller => "comments", :action => "update" })
+  
+  # DELETE
+  get("/delete_comment/:path_id", { :controller => "comments", :action => "destroy" })
+
+  #------------------------------
+
+  # Routes for the Photo resource:
+
+  # CREATE
+  post("/insert_photo", { :controller => "photos", :action => "create" })
+          
+  # READ
+  get("/photos", { :controller => "photos", :action => "index" })
+  
+  get("/photos/:path_id", { :controller => "photos", :action => "show" })
+  
+  # UPDATE
+  
+  post("/modify_photo/:path_id", { :controller => "photos", :action => "update" })
+  
+  # DELETE
+  get("/delete_photo/:path_id", { :controller => "photos", :action => "destroy" })
+
+  #------------------------------
+
 end
